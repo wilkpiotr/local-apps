d781ec8b642fe51dbb6be539c0186f0eb254ae57
UNIVERSE\msikora <maciej.sikora@dataart.com>
2018-03-27 00:48:16 +0200

    bugs and movies examples

---
+++ src/bugs.js
@@ -0,0 +1,5 @@
+console.log('I am bugs.js!');
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded. Ready to go!");
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: { 
+    bugs: './src/bugs.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-2']
+          }
+        }
+      }
+  ]},
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9001,
+    hot: true,
+    watchContentBase: true
+  }
+};
